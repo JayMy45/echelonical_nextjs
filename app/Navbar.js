0590b7841fb2198d6d55ffc3de650cff8f01bd03
Jeremy Myers <echelonical@gmail.com>
2023-06-09 17:49:51 -0500

    updates to NavBar Howdie and Layout.  Added footer

--- app/Navbar.js
+++ app/Navbar.js
@@ -17,8 +17,8 @@ export const Navbar = () => {
                     <ul className="nav__list">
                         <li className="nav__item"><a href="#home" className="nav__link">Home</a></li>
                         <li className="nav__item"><a href="#services" className="nav__link">My Services</a></li>
-                        <li className="nav__item"><a href="#" className="nav__link">About Me</a></li>
-                        <li className="nav__item"><a href="#" className="nav__link">My Work</a></li>
+                        <li className="nav__item"><a href="#about" className="nav__link">About Me</a></li>
+                        <li className="nav__item"><a href="#work" className="nav__link">My Work</a></li>
                     </ul>
                 </nav>
             </header>
