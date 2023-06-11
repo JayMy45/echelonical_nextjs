0da20f7b5066f0afeffca059e3a97d4d96a80118
Jeremy Myers <echelonical@gmail.com>
2023-06-11 13:08:41 -0500

    minor update to layout.js

--- app/page.js
+++ app/page.js
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        {/* <Howdie />
+        <Howdie />
         <Services />
-        <AboutMe /> */}
+        <AboutMe />
 
       </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
