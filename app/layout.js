68f6e5f7935dc6cab89b2ac0eca77839961cad10
Jeremy Myers <echelonical@gmail.com>
2023-06-11 12:50:48 -0500

    fixed bug with NextJs rounter issues

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Navbar } from './components/navbar/Navbar.js'
 import Footer from './components/footer/footer.js'
-import './globals.css'
 import { Inter } from 'next/font/google'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,10 +12,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <div>
-      <Navbar />
-      <body className={inter.className}>{children}</body>
-      <Footer />
-    </div>
+    <html>
+      <body className={inter.className}>
+        <Navbar />
+
+        {children}
+        <Footer />
+      </body>
+    </html>
   )
 }
