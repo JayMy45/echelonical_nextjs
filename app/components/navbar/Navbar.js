d892d87ca71c7265614f93a2e7a5fc3ef42832a5
Jeremy Myers <echelonical@gmail.com>
2023-06-11 14:36:53 -0500

    updates to navbar style. add cloudinary to application from npm

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -4,24 +4,25 @@ import Link from "next/link"
 export const Navbar = () => {
     return (
         <>
-            <header>
+            <nav>
                 <div className="logo">
-                    <img src="" alt="" />
+                    <Image
+                        src='https://res.cloudinary.com/dp04hh5pz/image/upload/ar_1:1,b_rgb:262c35,bo_5px_solid_rgb:f1eaea,c_fill,g_auto,r_max,w_1000/v1675295594/HoustonRocket.jpg'
+                        alt="picture of ninja"
+                        width={128}
+                        height={77}
+                    />
+
+                    <button className="nav__toggle" aria-label="toggle navigation">
+                        <span className="hamburger"></span>
+                    </button>
                 </div>
-                <button className="nav__toggle" aria-label="toggle navigation">
-                    <span className="hamburger"></span>
-                </button>
 
-
-                <nav className="nav">
-                    <ul className="nav__list">
-                        <li className="nav__item"><a href="#home" className="nav__link">Home</a></li>
-                        <li className="nav__item"><a href="#services" className="nav__link">My Services</a></li>
-                        <li className="nav__item"><a href="#about" className="nav__link">About Me</a></li>
-                        <li className="nav__item"><a href="#work" className="nav__link">My Work</a></li>
-                    </ul>
-                </nav>
-            </header>
+                <Link href="#home" className="nav__link">Home</Link>
+                <Link href="#services" className="nav__link">My Services</Link>
+                <Link href="#about" className="nav__link">About Me</Link>
+                <Link href="#work" className="nav__link">My Work</Link>
+            </nav>
         </>
     )
 }
