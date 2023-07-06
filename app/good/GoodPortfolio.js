54ec1e42f9a4fece0d36d7a3b4e1de02c51cddcc
Jeremy Myers <echelonical@gmail.com>
2023-07-05 19:49:06 -0500

    updates to portfolio on homepage layout

--- app/good/GoodPortfolio.js
+++ app/good/GoodPortfolio.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function GoodPortfolio() {
     return (
         <>
-            <div className="grid grid-cols-2 gap-x-7 max-w-xl  overflow-hidden mx-auto mt-10 p-4">
+            <div className="grid grid-cols-2 gap-x-7 max-w-xl  overflow-hidden mx-auto mt-10 p-5">
 
                 <div className="border-2 rounded-lg bg-white shadow-lg p-3">
                     <Link className="" href="/pages/Done">
@@ -15,8 +15,8 @@ export default function GoodPortfolio() {
                             src="https://res.cloudinary.com/dp04hh5pz/image/upload/v1687918473/Echelonical/DONE-LOGO_3_abwlln.png"
                             alt="image of full stack capstone"
                         />
-                        <span className="bg-gray-200 p-1 text-gray-700 mb-5 ml-4 rounded-lg font-bold" >
-                            Demo
+                        <span className="bg-green-600 p-1 text-white mb-5 ml-4 rounded-lg font-bold" >
+                            Demo Project
                         </span>
                     </Link>
                 </div>
@@ -30,8 +30,8 @@ export default function GoodPortfolio() {
                             src="https://res.cloudinary.com/dp04hh5pz/image/upload/v1687918656/Echelonical/Music_City_2_zpaokx.png"
                             alt="image of full stack capstone"
                         />
-                        <span className="bg-gray-200 p-1 text-gray-700 mb-5 ml-4 rounded-lg font-bold" >
-                            Demo
+                        <span className="bg-orange-200 p-1 text-gray-700 mb-5 ml-4 rounded-lg font-bold" >
+                            Demo Project
                         </span>
                     </Link>
                 </div>
