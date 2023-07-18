d046b70237342c71296080b8db886a234323ffb1
Jeremy Myers <echelonical@gmail.com>
2023-07-18 14:24:25 -0500

    made picture visible on About Me page

--- app/pages/aboutme/BannerAboutMe.js
+++ app/pages/aboutme/BannerAboutMe.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function BannerAboutMe() {
 
 
@@ -7,14 +9,16 @@ export default function BannerAboutMe() {
                 <div className="py-3 md:py-32 h-full rounded-xl m-3" style={{
                     backgroundImage: "linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url('https://res.cloudinary.com/dp04hh5pz/image/upload/v1687974820/Echelonical/IMG_0672_kkhlbm.jpg')",
                     backgroundSize: "cover",
-                    backgroundPosition: "center"
+                    backgroundPosition: "bottom 5px"
                 }}>
-                    <div className="container mx-auto px-6 mt-40">
-                        <h2 className="text-5xl font-bold text-white mb-2 uppercase">About Me</h2>
-                        <h3 className="text-2xl mb-8 text-gray-200">Jeremy Myers</h3>
-                        <button className="text-white uppercase border-2 px-8 py-4 font-bold tracking-wider shadow-lg">
+                    <div className="container mx-auto px-6 mt-44">
+                        <div className="">
+                            <h1 className="text-5xl font-bold text-white mb-2 uppercase">About Me</h1>
+                            <h3 className="text-2xl mb-8 text-gray-200">Jeremy Myers</h3>
+                        </div>
+                        <Link href="pages/portfolio" className="text-white uppercase border-2 px-8 py-4 font-bold tracking-wider shadow-lg">
                             Explore Projects
-                        </button>
+                        </Link>
                     </div>
                 </div>
             </div>
