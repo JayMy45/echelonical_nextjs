71c29424dd5383f4b1349ee60713bacfec6c67d4
Jeremy Myers <echelonical@gmail.com>
2023-08-29 22:16:23 -0500

    update to favicon.ico  Added dynamic routing setup

---
+++ app/pages/skills/[id]/page.js
@@ -0,0 +1,9 @@
+export default function Page({ params }) {
+    return (
+        <>
+            <div>
+                <h2>detailed skills {params.id} </h2>
+            </div>
+        </>
+    )
+}
