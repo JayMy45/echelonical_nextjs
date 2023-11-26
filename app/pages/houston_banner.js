d6751dda40690b02a4f17574e28fbd87976535c5
Jeremy Myers <echelonical@gmail.com>
2023-11-25 20:42:40 -0600

    added new picture of houston

--- app/pages/houston_banner.js
+++ app/pages/houston_banner.js
@@ -1,16 +1,29 @@
+import Image from "next/image";
+
 export default function HoustonBanner() {
 
 
     return (
         <>
-            <div className="py-32 h-full" style={{
-                backgroundImage: "linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url('https://res.cloudinary.com/dp04hh5pz/image/upload/v1689278073/Echelonical/2BEA0CED-E689-4477-8403-B780E25B6D6B_1_105_c_ao3fk2.jpg')",
-                backgroundSize: "cover",
-                backgroundPosition: "center"
-            }}>
-                <div className="container mx-auto px-6 mt-40">
-                    <h1 className='text-7xl'>Houston</h1>
+            <div className="grid grid-cols-2">
+                <div>
+                    <Image
+                        src='/HoustonPuppy.png'
+                        alt='Houston Puppy'
+                        width={400}
+                        height={400}
+                    />
                 </div>
+                <div className="py-32 h-full" style={{
+                    backgroundImage: "linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url('https://res.cloudinary.com/dp04hh5pz/image/upload/v1689278073/Echelonical/2BEA0CED-E689-4477-8403-B780E25B6D6B_1_105_c_ao3fk2.jpg')",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center"
+                }}>
+                    <div className="container mx-auto px-6 mt-40">
+                        <h1 className='text-7xl'>Houston</h1>
+                    </div>
+                </div>
+
             </div>
         </>
     )
